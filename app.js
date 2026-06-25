2dfa0c8790c2f5aaccc3b017c2cacf9015f0d633
Farhan <farhan@antiersolutions.com>
2026-06-25 12:41:22 +0530

    exploring GET, POST , DELETE http methods

--- app.js
+++ app.js
@@ -2,15 +2,32 @@ const express = require('express')
 
 const app = express()
 
-app.use('/contact',
-    // (req,res) => {                        route handlers
-    // res.send("This is contact page")
-    // }
-)
+// app.use('/contact', (_req, res) => {
+//     res.send("This is contact page")
+// })
+// app.use('/',(req,res) => {
+//     res.send("This Home Page")
+// })
+
+
+// this will match only the GET method for the /user route
+app.get('/user', (req, res) => {
+    res.send({firstName: "Farhan", lastName: "Khan"})
+})
+
+app.post('/user', (req, res) => {
+    res.send("Data is saved successfully in database")
+})
+
+app.delete('/user', (req, res) => {
+    res.send("User Deleted successfully")
+})
+
+
+// this will match all the HTTP methods (GET, POST, PUT, DELETE, etc.) for the /user route
 app.use('/',(req,res) => {
     res.send("This Home Page")
 })
-
 
 app.listen(7777, () => {
     console.log("server is now running")
---
+++ practice.md
@@ -0,0 +1,7 @@
+
+
+
+
+
+
+- write logic to handle GET, POST , PUT , PATCH , DELETE calls and test them on postman 
